7c72248cc8e3916408b4b6e264ff2fb4e74bddba
Andranik Simonyan <andranik.s.s@gmail.com>
2018-02-23 00:09:16 +0200

    Add base layout for chat with material-ui

--- src/App.js
+++ src/App.js
@@ -1,19 +1,92 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from 'react';
+import { withStyles } from 'material-ui/styles';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import AccountCircle from 'material-ui-icons/AccountCircle';
+import Typography from 'material-ui/Typography';
+import Toolbar from 'material-ui/Toolbar';
 
-class App extends Component {
+const drawerWidth = 320;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    height: 430,
+    marginTop: theme.spacing.unit * 3,
+    zIndex: 1,
+    overflow: 'hidden'
+  },
+  appFrame: {
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%'
+  },
+  appBar: {
+    position: 'absolute',
+    width: `calc(100% - ${drawerWidth}px)`
+  },
+  'appBar-left': {
+    marginLeft: drawerWidth
+  },
+  'appBar-right': {
+    marginRight: drawerWidth
+  },
+  drawerPaper: {
+    position: 'relative',
+    height: '100%',
+    width: drawerWidth
+  },
+  drawerHeader: theme.mixins.toolbar,
+  content: {
+    backgroundColor: theme.palette.background.default,
+    width: '100%',
+    padding: theme.spacing.unit * 3,
+    height: 'calc(100% - 56px)',
+    marginTop: 56,
+    [theme.breakpoints.up('sm')]: {
+      height: 'calc(100% - 64px)',
+      marginTop: 64
+    }
+  }
+});
+
+class App extends React.Component {
   render() {
+    const { classes } = this.props;
+
     return (
-      <div className='App'>
-        <header className='App-header'>
-          <h1 className='App-title'>Welcome to React</h1>
-        </header>
-        <p className='App-intro'>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className={classes.appFrame}>
+        <AppBar className={classes.appBar}>
+          <Toolbar>
+            <Typography variant='title' color='inherit' noWrap>
+              dogecodes chat
+            </Typography>
+            <IconButton
+              aria-haspopup='true'
+              color='inherit'
+            >
+              <AccountCircle />
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        <Drawer
+          variant='permanent'
+          classes={{
+            paper: classes.drawerPaper
+          }}
+        >
+          <div className={classes.drawerHeader} />
+        </Drawer>
+        <main className={classes.content}>
+          <Typography>
+            {'You think water moves fast? You should see ice.'}
+          </Typography>
+        </main>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+// Styles
+import './index.css';
+import 'typeface-roboto';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
