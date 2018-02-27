d8cd59260a8f42dae4ba6ff5c8af35273c0bd659
Andranik Simonyan <andranik.s.s@gmail.com>
2018-02-27 14:37:17 +0200

    Refactoring color-from util

--- src/utils/color-from.js
+++ src/utils/color-from.js
@@ -1,31 +1,19 @@
-import red from 'material-ui/colors/red';
-import pink from 'material-ui/colors/pink';
-import purple from 'material-ui/colors/purple';
-import indigo from 'material-ui/colors/indigo';
-import blue from 'material-ui/colors/blue';
-import teal from 'material-ui/colors/teal';
-import green from 'material-ui/colors/green';
-import lightGreen from 'material-ui/colors/lightGreen';
-import amber from 'material-ui/colors/amber';
-import orange from 'material-ui/colors/orange';
-import deepOrange from 'material-ui/colors/deepOrange';
-import deepPurple from 'material-ui/colors/deepPurple';
-import blueGrey from 'material-ui/colors/blueGrey';
+import * as colors from 'material-ui/colors';
 
-const colors = [
-  red,
-  pink,
-  purple,
-  indigo,
-  blue,
-  teal,
-  green,
-  lightGreen,
-  amber,
-  orange,
-  deepOrange,
-  deepPurple,
-  blueGrey
+const colorNames = [
+  'red',
+  'pink',
+  'purple',
+  'indigo',
+  'blue',
+  'teal',
+  'green',
+  'lightGreen',
+  'amber',
+  'orange',
+  'deepOrange',
+  'deepPurple',
+  'blueGrey'
 ];
 
 export default function colorFrom(string) {
@@ -36,11 +24,10 @@ export default function colorFrom(string) {
       .map(char => char.charCodeAt())
       .reduce((sum, num) => sum + num, 0);
 
-    const colorIndex = index % colors.length;
-
-    return colors[colorIndex][500];
+    const colorIndex = index % colorNames.length;
+    return colors[colorNames[colorIndex]][500];
   } catch (e) {
     console.error(e);
-    return blueGrey[500];
+    return colors['blueGrey'][500];
   }
 }
