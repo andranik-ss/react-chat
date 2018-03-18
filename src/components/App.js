34c5f590300cd2eb4de27103cc69217a032cbcfc
work\asimonyan <andranik.s.s@gmail.com>
2018-03-18 23:50:25 +0200

    Fix saving redux store after HMR

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
 import { Router, Route, Switch, Redirect } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import PrivateRoute from '../containers/PrivateRoute';
 import ChatPage from '../containers/ChatPage';
 import WelcomePage from '../containers/WelcomePage';
 import history from '../utils/history';
-import configureStore from '../store';
-
-const store = configureStore();
 
 const App = () => {
   return (
-    <Provider store={store}>
-      <Router history={history}>
-        <Switch>
-          <Route exact path='/(welcome)?' component={WelcomePage} />
-          <PrivateRoute path='/chat/:chatId?' component={ChatPage} />
-          <Redirect to='/' />
-        </Switch>
-      </Router>
-    </Provider>
+    <Router history={history}>
+      <Switch>
+        <Route exact path='/(welcome)?' component={WelcomePage} />
+        <PrivateRoute path='/chat/:chatId?' component={ChatPage} />
+        <Redirect to='/' />
+      </Switch>
+    </Router>
   );
 };
 
--- src/reducers/messages.js
+++ src/reducers/messages.js
@@ -5,6 +5,7 @@ const initialState = [];
 export default (state = initialState, action) => {
   switch (action.type) {
     case types.SEND_MESSAGE_SUCCESS:
+    case types.JOIN_CHAT_SUCCESS:
       return [...state, action.payload.message];
     case types.FETCH_CHAT_SUCCESS:
       return action.payload.chat.messages;
