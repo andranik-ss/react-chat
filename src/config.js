4889d5c27b13b5b1bd745fc923d7907a0de4e46f
Andranik Simonyan <andranik.s.s@gmail.com>
2018-03-25 18:14:35 +0300

    fix server adress

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const PROD = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  API_URI: PROD ? 'https://dogecodes-chat-api.herokuapp.com/' : 'http://localhost:8000',
+  API_URI: PROD ? 'https://dogecodes-chat-api.herokuapp.com' : 'http://localhost:8000',
   API_VERSION: 'v1',
   SOCKETS_URI: PROD ? 'wss://dogecodes-chat-api.herokuapp.com/' : 'ws://localhost:8000/',
 };
